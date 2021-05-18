467fe9d1c6df5bce5d70fbfe4296bbfb40810b22
Jayant Katia <jayantkatia65@gmail.com>
2021-05-18 16:21:19 +0530

    Update vpn-logout.sh

--- scripts/vpn-logout.sh
+++ scripts/vpn-logout.sh
@@ -2,7 +2,7 @@
 
 # endScript runs after every name+tag
 # disconnect & logout at the end of last iteration 
-if [[ $3 -eq 'CA' ]]
+if [[ "$3" == "CA" ]]
 then
     windscribe disconnect
     windscribe logout
